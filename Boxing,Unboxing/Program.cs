f44a411fe8fb8d454d0209558f152d7c26cd6f0c
Jakub Kwaśniak <98922994+KwasniakJakub@users.noreply.github.com>
2022-09-13 12:36:08 +0200

    Add files via upload

---
+++ Boxing,Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1); //Dzieje się boks
+            list.Add("Mosh");
+            list.Add(DateTime.Today);  //Dzieje się boks
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //var number = (int)list[1]; 
+            var anotherList = new List<int>();
+            //anotherList.Add(1);
+            var names = new List<string>();
+            names.Add("Jakub");
+        }
+    }
+}
